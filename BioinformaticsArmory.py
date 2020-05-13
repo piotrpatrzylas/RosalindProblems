73f3bdf76fcbdf3872574755cc63f0e9c4fba7d6
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-05-13 20:48:34 +0100

    Bioinformatics Armory update.

--- BioinformaticsArmory.py
+++ BioinformaticsArmory.py
@@ -3,3 +3,9 @@ from Bio.Seq import Seq
 my_seq = Seq(open('rosalind_ini.txt', 'r').read())
 print(my_seq.count("A"), my_seq.count("C"), my_seq.count("G"), my_seq.count("T"))
 
+# GenBank Introduction
+from Bio import Entrez
+Entrez.email = "piotrpatrzylas@gmail.com"
+handle = Entrez.esearch(db="nucleotide", term='"Ananas"[Organism] AND "2006/01/05"[PDAT] : "2010/08/17"[PDAT]')
+record = Entrez.read(handle)
+print(record["Count"])
