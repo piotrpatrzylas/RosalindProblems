989c1634d03c8d6e10c6f562aa51d3b9aed6abef
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-05-19 21:08:38 +0100

    Algorithmic Heights update.

---
+++ AlgorithmicHeights.py
@@ -0,0 +1,8 @@
+# Fibonacci Numbers
+def fibo(n):
+    if n == 0:
+        return 0
+    array = [0, 1]
+    for i in range(2, n+1):
+        array.append(array[i-1] + array[i-2])
+    return array[n]
