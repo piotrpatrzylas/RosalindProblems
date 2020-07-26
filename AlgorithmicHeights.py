622310d86cf3e20c5957116a239b38af9e5e6a8e
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-07-26 08:06:20 +0100

    Algorithmic Heights update.

--- AlgorithmicHeights.py
+++ AlgorithmicHeights.py
@@ -9,10 +9,12 @@ def fibo(n):
 
 # Binary Search
 
+
 file = open("path-to-a-file", "r")
 msg = file.read().splitlines()
 msg[0], msg[1] = int(msg[0]), int(msg[1])
 msg[2], msg[3] = list(map(int, msg[2].split())), list(map(int, msg[3].split()))
+
 
 def bsearch(Arr, low, high, el):
     if high >= low:
@@ -26,12 +28,14 @@ def bsearch(Arr, low, high, el):
     else:
         return -1
 
+
 def arrbsearch(n, m, Arrn, Arrm):
     results = []
     for i in range(m):
         results += [bsearch(Arrn, 0, len(Arrn)-1, Arrm[i])]
     results = [x if x == -1 else x + 1 for x in results]
     print (*results)
+
 
 arrbsearch(msg[0], msg[1], msg[2], msg[3])
 
@@ -62,6 +66,35 @@ def insort(n, arr):
             arr[k], arr[k-1] = arr[k-1], arr[k]
             k -= 1
             counter += 1
-    print (counter)
+    print(counter)
 
 insort(n, arr)
+
+# Merge Two Sorted Arrays
+file = open("path-to-a-file", "r")
+msg = file.read().splitlines()
+arr1, arr2 = list(map(int, msg[1].split())), list(map(int, msg[3].split()))
+n, m = int(msg[0]), int(msg[2])
+
+
+def mergesorted(n, arr1, m, arr2):
+    c1 = 0
+    c2 = 0
+    arr3 = []
+    while c1 < n and c2 < m:
+        if arr1[c1] <= arr2[c2]:
+            arr3.append(arr1[c1])
+            c1 += 1
+        else:
+            arr3.append(arr2[c2])
+            c2 += 1
+    while c1 < n:
+        arr3.append(arr1[c1])
+        c1 += 1
+    while c2 < m:
+        arr3.append(arr2[c2])
+        c2 += 1
+    print(*arr3)
+
+
+mergesorted(n, arr1, m, arr2)
